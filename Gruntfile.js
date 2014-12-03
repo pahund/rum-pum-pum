a484bce6d2895fb88fe9da8aa400f2df6ae70886
Patrick Hund <pahund@ebay.com>
2014-12-03 17:47:51 +0100

    added Grunt build pipeline and Bower component management
    Change-Id: Ia05150c82e08692adee7d99dd8a94c598929eafa

---
+++ Gruntfile.js
@@ -0,0 +1,116 @@
+/**
+ * Gruntfile
+ *
+ * @author <a href="mailto:pahund@team.mobile.de">Patrick Hund</a>
+ * @since 03/12/14
+ */(function () {
+    "use strict";
+
+    module.exports = function (grunt) {
+        var bowerManaged;
+
+        /*
+         * Files that are provided by Bower, deployed into lib directories by Grunt
+         */
+        bowerManaged = {
+            js: [
+                "jquery/dist/jquery.js",
+                "requirejs/require.js",
+                "pixi/bin/pixi.dev.js"
+            ]
+        };
+
+        // Project configuration.
+        grunt.initConfig({
+            clean: [
+                "bower_components",
+                ".jshintrc",
+                "js/lib"
+            ],
+            jshint: {
+                options: {
+                    jshintrc: true
+                },
+                all: [
+                    "js/**/*.js",
+                    "!js/lib/**/*.js"
+                ]
+            },
+
+            jscs: {
+                options: {
+                    requireMultipleVarDecl: "onevar"
+                },
+                all: [
+                    "js/**/*.js",
+                    "!js/lib/**/*.js"
+                ]
+            },
+
+            watch: {
+                options: {
+                    livereload: true
+                },
+                html: {
+                    files: ["index.html"]
+                },
+                css: {
+                    files: ["css/**/*.css"]
+                },
+                js: {
+                    files: [
+                        "js/**/*.js",
+                        "!js/lib/**/*.js"
+                    ],
+                    tasks: [
+                        "jshint",
+                        "jscs"
+                    ]
+                }
+            },
+
+            copy: {
+                jshintrc: {
+                    src: "bower_components/mde-jshintrc/linters/jshintrc",
+                    dest: ".jshintrc"
+                },
+                bower_js: {
+                    src: bowerManaged.js,
+                    cwd: "bower_components",
+                    dest: "js/lib",
+                    expand: true,
+                    flatten: true
+                }
+            }
+        });
+
+        grunt.loadNpmTasks("grunt-contrib-jshint");
+        grunt.loadNpmTasks("grunt-jscs-checker");
+        grunt.loadNpmTasks("grunt-contrib-watch");
+        grunt.loadNpmTasks("grunt-contrib-clean");
+        grunt.loadNpmTasks("grunt-contrib-copy");
+
+        grunt.registerTask("help", function () {
+            grunt.log.writeln("grunt help:      displays this message");
+            grunt.log.writeln("grunt test:      checks JavaScript code style with JSHint and JSCS");
+            grunt.log.writeln("grunt install:   copies libs from bower_components, deploys .jshintrc");
+            grunt.log.writeln("grunt watch:     checks JavaScript code style when JS files are changed");
+            grunt.log.writeln("grunt clean:     deletes downloaded or generated files and directories");
+        });
+
+        grunt.registerTask("default", [
+            "install",
+            "test"
+        ]);
+
+        grunt.registerTask("install", [
+            "copy"
+        ]);
+
+        grunt.registerTask("test", [
+            "jshint",
+            "jscs"
+        ]);
+    };
+}());
+
