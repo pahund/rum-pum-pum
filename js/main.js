b58ad8fdbb854f33beecce37d629d8afc8ee6930
Patrick Hund <pahund@ebay.com>
2014-12-03 19:36:00 +0100

    flapping bird!
    Change-Id: I6f71088c2a04e385ea2a1957ffa50a0ccc29597b

--- js/main.js
+++ js/main.js
@@ -18,12 +18,21 @@ require.config({
 require([
     "jquery",
     "app/player",
-    "app/Context"
-], function ($, player) {
+    "pixi.dev"
+], function ($, player, PIXI) {
+
+    var win = {
+            w: $(window).width(),
+            h: $(window).height()
+        },
+        stage,
+        renderer,
+        texture,
+        bird,
+        flapper;
 
     function playFactory(name) {
         return function () {
-            console.log("[PH_LOG] name: ", name); // PH_TODO: REMOVE
             player.play(name);
         };
     }
@@ -35,6 +44,68 @@ require([
         player.load(name, file);
         $button.on("click", playFactory(name));
     });
+
+    stage = new PIXI.Stage(0xFFFFFF);
+    renderer = PIXI.autoDetectRenderer(win.w, win.h);
+    texture = PIXI.Texture.fromImage("images/flapping-bird.png");
+    texture.setFrame(new PIXI.Rectangle(0, 0, 395, 419));
+    bird = new PIXI.Sprite(texture);
+    bird.anchor.x = 0.5;
+    bird.anchor.y = 0.5;
+    bird.position.x = win.w / 2;
+    bird.position.y = win.h / 2;
+    stage.addChild(bird);
+    $("body").append(renderer.view);
+
+    function intervalExecutorFactory(func, interval) {
+        var startTime;
+        function getTimestamp() {
+            return new Date().getTime();
+        }
+        if (typeof func !== "function") {
+            throw "Error: intervalExecutorFacotry expects function to be executed as first argument";
+        }
+        if (interval === undefined) {
+            interval = 1000; // default interval 1 sec
+        }
+        startTime = getTimestamp();
+        return function () {
+            var currentTime = getTimestamp();
+            if (currentTime > startTime + interval) {
+                func();
+                startTime = getTimestamp();
+            }
+        };
+    }
+
+    function phasedExecutorFactory(func, numberOfPhases) {
+        var phase = 0;
+        if (typeof func !== "function") {
+            throw "Error: phasedExecutorFactory expects function to be executed as first argument";
+        }
+        if (typeof numberOfPhases !== "number") {
+            throw "Error: phasedExecutorFactory expects number of phases as second argument";
+        }
+        return function () {
+            func(phase);
+            if (++phase === numberOfPhases) {
+                phase = 0;
+            }
+        };
+    }
+
+    function flap(phase) {
+        texture.setFrame(new PIXI.Rectangle(phase * 395, 0, 395, 419));
+    }
+
+    flapper = intervalExecutorFactory(phasedExecutorFactory(flap, 3), 100);
+
+    (function animloop(){
+        requestAnimationFrame(animloop);
+        flapper();
+        renderer.render(stage);
+    })();
+
 });
 
 
