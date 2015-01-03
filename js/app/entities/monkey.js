521664873644612749a9a96f8349480cc78ed9aa
Patrick Hund <pahund@ebay.com>
2015-01-03 13:58:21 +0100

    variable anchors for entities, placed bear and monkey lower
    Change-Id: I964a60c1d047557c121df4ad502ec5c128a6712b

--- js/app/entities/monkey.js
+++ js/app/entities/monkey.js
@@ -30,7 +30,8 @@ define(function (require) {
                 coordinates: new PIXI.Point(
                     input.x === undefined ? dimensions.viewport.w / 2 : input.x,
                     input.y === undefined ? dimensions.viewport.h / 2 : input.y),
-                scale: new PIXI.Point(scale, scale)
+                scale: new PIXI.Point(scale, scale),
+                anchor: new PIXI.Point(0.5, 0.1)
             }),
             proximityListener({
                 action: function (trigger, listener) {
